ab58ec006ac4562d2ae6dba66261c64a68a244e9
tofof <github@tofof.com>
2022-04-26 18:29:19 -0500

    interrupt-based rotary encoder sketch, modified for no PIND register to read directly

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,55 @@
 #include <Arduino.h>
 
-const int buttonPin = 4;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+/*******Interrupt-based Rotary Encoder Sketch*******
+by Simon Merrett, based on insight from Oleg Mazurov, Nick Gammon, rt, Steve Spence
+*/
 
-int buttonState = 0;         // variable for reading the pushbutton status
+const static int pinA = 2; // Our first hardware interrupt pin is digital pin 2
+const static int pinB = 3; // Our second hardware interrupt pin is digital pin 3
+volatile byte aFlag = 0; // lets us know when we're expecting a rising edge on pinA to signal that the encoder has arrived at a detent
+volatile byte bFlag = 0; // lets us know when we're expecting a rising edge on pinB to signal that the encoder has arrived at a detent (opposite direction to when aFlag is set)
+volatile byte encoderPos = 0; //this variable stores our current value of encoder position. Change to int or uin16_t instead of byte if you want to record a larger range than 0-255
+volatile byte oldEncPos = 0; //stores the last encoder position value so we can compare to the current reading and see if it has changed (so we know when to print to the serial monitor)
+volatile int stateA = 0; //somewhere to store the raw value we read off the pin
+volatile int stateB = 0;
 
 void setup() {
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
+  pinMode(pinA, INPUT_PULLUP); // set pinA as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
+  pinMode(pinB, INPUT_PULLUP); // set pinB as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
+  attachInterrupt(0,PinA,RISING); // set an interrupt on PinA, looking for a rising edge signal and executing the "PinA" Interrupt Service Routine (below)
+  attachInterrupt(1,PinB,RISING); // set an interrupt on PinB, looking for a rising edge signal and executing the "PinB" Interrupt Service Routine (below)
+  Serial.begin(115200); // start the serial monitor link
 }
 
-void loop() {
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
+void PinA(){
+  noInterrupts(); //stop interrupts happening before we read pin values
+  stateA = digitalRead(pinA);
+  stateB = digitalRead(pinB);
+  if(stateA == HIGH && stateB == HIGH) { //check that we have both pins at detent (HIGH) and that we are expecting detent on this pin's rising edge
+    encoderPos --; //decrement the encoder's position count
+    bFlag = 0; //reset flags for the next turn
+    aFlag = 0; //reset flags for the next turn
+  }
+  else if (stateA == HIGH && stateB == LOW) bFlag = 1; //signal that we're expecting pinB to signal the transition to detent from free rotation
+  interrupts(); //restart interrupts
+}
 
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-  } else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
+void PinB(){
+  noInterrupts(); //stop interrupts happening before we read pin values
+  stateA = digitalRead(pinA);
+  stateB = digitalRead(pinB);
+  if (stateA == HIGH && stateB == HIGH && bFlag) { //check that we have both pins at detent (HIGH) and that we are expecting detent on this pin's rising edge
+    encoderPos ++; //increment the encoder's position count
+    bFlag = 0; //reset flags for the next turn
+    aFlag = 0; //reset flags for the next turn
+  }
+  else if (stateA == LOW && stateB == HIGH) aFlag = 1; //signal that we're expecting pinA to signal the transition to detent from free rotation
+  interrupts(); //restart interrupts
+}
+
+void loop(){
+  if(oldEncPos != encoderPos) {
+    Serial.println(encoderPos);
+    oldEncPos = encoderPos;
   }
 }
